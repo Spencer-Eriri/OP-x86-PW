37fdd5541e2466c4533cc58cec32eb60e0dcdbe0
Spencer-Eriri <78525581+Spencer-Eriri@users.noreply.github.com>
2026-02-04 23:34:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,11 +17,11 @@
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 
-#将上述删除的程序替换为 jerrykuku 版（18.06 分支）
+#将上述删除的程序替换为 jerrykuku 版（main 分支）
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/downloads/luci-app-argon-config
 
-#应用默认主题为 Argon-18.06
-#方式是取消 Bootstrap 在 Openwrt 中的依赖地位，取而代之的是 Argon-18.06
-#但 Bootstrap 和 Argon-18.06 均会编译，在 config 中去除 Bootstrap 是无效的
+#应用默认主题为 Argon（主线分支）
+#方式是取消 Bootstrap 在 Openwrt 中的依赖地位，取而代之的是 Argon（主线分支）
+#但 Bootstrap 和 Argon 均会编译，在 config 中去除 Bootstrap 是无效的
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
